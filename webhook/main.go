b9847119252139084ecc2d8ef72af5a18afb7e56
Akinnurun Oluwafemi <akinnurun.samuel@gmail.com>
2020-07-19 23:36:56 +0100

    update argo

--- webhook/main.go
+++ webhook/main.go
@@ -1,50 +1,72 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "github.com/buger/jsonparser"
-    "github.com/gin-gonic/gin"
-    "io/ioutil"
-    "os/exec"
-    "strconv"
-    "strings"
-    "time"
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/buger/jsonparser"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    gin.DisableConsoleColor()
-    gin.SetMode(gin.ReleaseMode)
+	gin.DisableConsoleColor()
+	gin.SetMode(gin.ReleaseMode)
 
-    r := gin.Default()
+	r := gin.Default()
 
-    r.POST("/", func(c *gin.Context) {
-        c.JSON(202, gin.H{})
+	r.POST("/", func(c *gin.Context) {
+		c.JSON(202, gin.H{})
 
-        buf := new(bytes.Buffer)
-        _, _ = buf.ReadFrom(c.Request.Body)
-        
-        gitRepo,_ := jsonparser.GetString(buf.Bytes(), "repository", "full_name")
-        gitRevision,_ := jsonparser.GetString(buf.Bytes(), "push", "changes", "[0]", "new", "name")
-        
-        gitRepoName := strings.Split(gitRepo, "/")[1]
-        fullGitRepo := "git@bitbucket.org:" + gitRepo + ".git"
-        timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(c.Request.Body)
 
-        argoFilename := "argo" + timestamp + ".yml"
+		gitRepo, _ := jsonparser.GetString(buf.Bytes(), "repository", "full_name")
+		appName, _ := jsonparser.GetString(buf.Bytes(), "repository", "name")
+		gitRevision, _ := jsonparser.GetString(buf.Bytes(), "push", "changes", "[0]", "new", "name")
 
-        input, _ := ioutil.ReadFile("argo.yml")
-        temp := bytes.Replace(input, []byte("<git_repo_name>"), []byte(gitRepoName), -1)
-        temp2 := bytes.Replace(temp, []byte("<git_repo_full>"), []byte(fullGitRepo), -1)
-        output := bytes.Replace(temp2, []byte("<git_revision>"), []byte(gitRevision), -1)
-        _ = ioutil.WriteFile(argoFilename, output, 0666)
+		if gitRevision == "" {
+			return
+		}
+		fmt.Println("repo: ", gitRepo)
+		fmt.Println("ref: ", gitRevision)
+		fmt.Println("name: ", appName)
 
-        commandOutput, err := exec.Command("sh", "-c", "./argo submit " + argoFilename).CombinedOutput()
-        if err != nil {
-            fmt.Printf("Accepted webhook request, did NOT start Argo workflow: git_repo=%q,git_revision=%q, because of: %q\n", gitRepo, gitRevision, string(err.Error()))
-        } else {
-            fmt.Printf("Accepted webhook request, started Argo workflow: git_repo=%q,git_revision=%q, with message: %q\n", gitRepo, gitRevision, string(commandOutput))
+		gitRepoName := strings.Split(gitRepo, "/")[1]
+		fullGitRepo := "git@bitbucket.org:" + gitRepo + ".git"
+		// timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+
+		// argoFilename := "argo" + timestamp + ".yml"
+		argoFilename := "argo_.yml"
+
+		items := []string{"staging", "master"}
+		_, found := Find(items, gitRevision)
+		if !found {
+			return
+		}
+
+
+		commandin := "argo submit " + argoFilename + " -p app-name=" + gitRepoName + " -p repo=" + fullGitRepo + " -p ref=" + gitRevision
+		fmt.Println(commandin)
+
+		commandOutput, err := exec.Command("sh", "-c", commandin).CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Printf("Accepted webhook request, did NOT start Argo workflow: git_repo=%q,git_revision=%q, because of: %q\n", gitRepo, gitRevision, string(err.Error()))
+		} else {
+			fmt.Printf("Accepted webhook request, started Argo workflow: git_repo=%q,git_revision=%q, with message: %q\n", gitRepo, gitRevision, string(commandOutput))
+		}
+	})
+	_ = r.Run(":3000")
+}
+
+
+func Find(slice []string, val string) (int, bool) {
+    for i, item := range slice {
+        if item == val {
+            return i, true
         }
-    })
-    _ = r.Run(":3000")
+    }
+    return -1, false
 }
